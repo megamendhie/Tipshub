bf2f0ce7c013db3878e29553a2d6b7102cfe0e27
Mendhie <imendhie@yahoo.com>
2019-06-08 11:56:59 +0100

    Added checkForUpdate method

--- app/src/main/java/com/sqube/tipshub/MainActivity.java
+++ app/src/main/java/com/sqube/tipshub/MainActivity.java
@@ -1,6 +1,9 @@
 package com.sqube.tipshub;
 
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,6 +15,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -22,6 +26,8 @@ import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
@@ -29,6 +35,11 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nullable;
 
@@ -44,9 +55,16 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     GoogleSignInClient mGoogleSignInClient;
     private FirebaseAuth auth;
     private FirebaseFirestore database;
-    FirebaseDatabase sysDatabase;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
     private FirebaseUser user;
     private Intent serviceIntent;
+
+    final int versionCode = BuildConfig.VERSION_CODE;
+    final String FB_RC_KEY_TITLE = "update_title";
+    final String FB_RC_KEY_DESCRIPTION = "update_description";
+    final String FB_RC_KEY_FORCE_UPDATE_VERSION = "force_update_version";
+    final String FB_RC_KEY_LATEST_VERSION = "latest_version";
+    final HashMap<String, Object> defaultMap = new HashMap<>();
 
     ActionBar actionBar;
     BottomNavigationView btmNav;
@@ -123,11 +141,12 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         serviceIntent = new Intent(this, UserDataFetcher.class);
         startService(serviceIntent);
         database = FirebaseFirestore.getInstance();
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         user = auth.getCurrentUser();
         userId = user.getUid();
-
         actionBar.setTitle("Home");
+        checkForUpdate();
         loadFragment();
         setHeader();
     }
@@ -252,4 +271,93 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
             auth.signOut();
         }
     }
+
+    private void checkForUpdate() {
+        // Hashmap which contains the default values for all the parameter defined in the remote config server
+        defaultMap.put(FB_RC_KEY_TITLE, "Update Available");
+        defaultMap.put(FB_RC_KEY_DESCRIPTION, "A new version of the application is available please click below to update the latest version.");
+        defaultMap.put(FB_RC_KEY_FORCE_UPDATE_VERSION, ""+versionCode);
+        defaultMap.put(FB_RC_KEY_LATEST_VERSION, ""+versionCode);
+
+        // To set the default values for the remote config parameters
+        mFirebaseRemoteConfig.setDefaults(defaultMap);
+        // To enable the developer mode
+        mFirebaseRemoteConfig.setConfigSettings(new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG).build());
+
+        Task<Void> fetchTask=mFirebaseRemoteConfig.fetch(BuildConfig.DEBUG?0: TimeUnit.HOURS.toSeconds(12));
+
+        fetchTask.addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    // After config data is successfully fetched, it must be activated before newly fetched
+                    // values are returned.
+                    mFirebaseRemoteConfig.activateFetched();
+                    Boolean visible = true;
+                    String title = mFirebaseRemoteConfig.getString(FB_RC_KEY_TITLE);
+                    String description = mFirebaseRemoteConfig.getString(FB_RC_KEY_DESCRIPTION);
+                    int forceUpdateVersion = Integer.parseInt(mFirebaseRemoteConfig.getString(FB_RC_KEY_FORCE_UPDATE_VERSION));
+                    int latestAppVersion = Integer.parseInt(mFirebaseRemoteConfig.getString(FB_RC_KEY_LATEST_VERSION));
+
+                    if (latestAppVersion > versionCode){
+                        if(forceUpdateVersion>versionCode)
+                            visible = false;
+                        updateAlert(title, description, visible);
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Fetch Failed",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+
+    private void updateAlert(String title, String description, boolean visible){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.update_alert, null);
+        builder.setView(dialogView).setCancelable(false);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+
+        Button btnUpdate = alertDialog.findViewById(R.id.btnUpdate);
+        Button btnLater = alertDialog.findViewById(R.id.btnLater);
+        btnLater.setVisibility(visible? View.VISIBLE : View.GONE);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rateApp();
+            }
+        });
+        btnLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                alertDialog.cancel();
+            }
+        });
+        TextView txtTitle = alertDialog.findViewById(R.id.txtTitle);
+        TextView txtDescription = alertDialog.findViewById(R.id.txtDescription);
+        txtTitle.setText(title);
+        txtDescription.setText(description);
+    }
+
+    public void rateApp(){
+        Uri uri = Uri.parse("market://details?id=" + getApplicationContext().getPackageName());
+        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+        // To count with Play market backstack, After pressing back button,
+        // to taken back to our application, we need to add following flags to intent.
+
+        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
+                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
+                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
+        try {
+            startActivity(goToMarket);
+        } catch (ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW, uri));
+        }
+    }
+
 }
