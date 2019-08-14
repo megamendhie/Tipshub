24183230892da9396bb14dc8d60b65e226ba877f
Mendhie <imendhie@yahoo.com>
2019-08-14 11:09:43 +0100

    News loading algo modified

--- app/src/main/java/com/sqube/tipshub/NewsStoryActivity.java
+++ app/src/main/java/com/sqube/tipshub/NewsStoryActivity.java
@@ -3,9 +3,11 @@ package com.sqube.tipshub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class NewsStoryActivity extends AppCompatActivity {
 
@@ -14,10 +16,11 @@ public class NewsStoryActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_story);
         WebView webView = findViewById(R.id.wvNews);
-        //ProgressBar progressBar;
+        ProgressBar progressBar = findViewById(R.id.progressBar);
 
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
+        Log.i("News", "onCreate: "+url);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.getSettings().setDisplayZoomControls(false);
         webView.getSettings().setUseWideViewPort(true);
@@ -27,6 +30,7 @@ public class NewsStoryActivity extends AppCompatActivity {
         webView.getSettings().setSupportMultipleWindows(true);
 
         webView.setWebViewClient(new WebViewClient(){
+
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
