1df62a69efa3dc27a2b9a7967698fc811c98e009
Mendhie <imendhie@yahoo.com>
2020-01-28 19:29:31 +0100

    Updated HTTP request function

---
+++ app/src/main/java/com/sqube/tipshub/FullViewActivity.java
@@ -0,0 +1,14 @@
+package com.sqube.tipshub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FullViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_view);
+    }
+}
--- app/src/main/java/utils/HttpConFunction.java
+++ app/src/main/java/utils/HttpConFunction.java
@@ -6,9 +6,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class NewsFunction {
+public class HttpConFunction {
 
-    public static String excuteGet(String targetURL, String urlParameters) {
+    public static String excuteGet(String targetURL, String requestingFragment) {
         URL url;
         HttpURLConnection connection = null;
         try {
@@ -17,8 +17,14 @@ public class NewsFunction {
             connection = (HttpURLConnection) url.openConnection();
             connection.setRequestProperty("content-type", "application/json;  charset=utf-8");
 
+            if(requestingFragment.equals("HOME")){
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("x-rapidapi-host",  "football-prediction-api.p.rapidapi.com");
+                connection.setRequestProperty("x-rapidapi-key", "894508aa72msha7ba8fc97347ac4p13bc3djsn981a951e01ff");
+            }
+            else
+                connection.setRequestProperty("Content-Language", "en-US");
 
-            connection.setRequestProperty("Content-Language", "en-US");
 
             //connection.setUseCaches(true);
             connection.setDoInput(true);
