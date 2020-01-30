482f581b1f409ace3403506c3ac8a8828999707a
Mendhie <imendhie@yahoo.com>
2020-01-30 23:04:09 +0100

    Bugs fixed

--- app/src/main/java/com/sqube/tipshub/AccountActivity.java
+++ app/src/main/java/com/sqube/tipshub/AccountActivity.java
@@ -1,6 +1,8 @@
 package com.sqube.tipshub;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,16 +12,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import adapters.SubscriberAdapter;
 import adapters.SubscriptionAdapter;
 
 public class AccountActivity extends AppCompatActivity {
     private static final String TAG = "AccActivity";
+    private SubscriberAdapter subscriberAdapter;
+    private SubscriptionAdapter subscriptionAdapter;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -52,27 +59,32 @@ public class AccountActivity extends AppCompatActivity {
         Query querySubscriptions = database.collection("subscriptions").orderBy("timestamp", Query.Direction.DESCENDING)
                 .whereEqualTo("subFromId", userId);
 
-        SubscriberAdapter subscriberAdapter = new SubscriberAdapter(querySubscribers, getApplicationContext());
-        SubscriptionAdapter subscriptionAdapter = new SubscriptionAdapter(querySubscriptions, getApplicationContext());
+
+        querySubscribers.get().addOnCompleteListener(task -> {
+            if(task.getResult()==null || task.getResult().isEmpty())
+                txtDisplay1.setVisibility(View.VISIBLE);
+
+        });
+        querySubscriptions.get().addOnCompleteListener(task -> {
+            if(task.getResult()==null || task.getResult().isEmpty())
+                txtDisplay2.setVisibility(View.VISIBLE);
+
+        });
+
+        subscriberAdapter = new SubscriberAdapter(querySubscribers, getApplicationContext());
+        subscriptionAdapter = new SubscriptionAdapter(querySubscriptions, getApplicationContext());
         listSubscribers.setAdapter(subscriberAdapter);
         listSubscriptions.setAdapter(subscriptionAdapter);
-        if(subscriberAdapter!=null){
-            Log.i(TAG, "subscriberAdapter: started listening");
-            subscriberAdapter.startListening();
-        }
-        if(subscriptionAdapter!=null){
-            Log.i(TAG, "subscriptionAdapter: started listening");
-            subscriptionAdapter.startListening();
-        }
+        Log.i(TAG, "subscriberAdapter: started listening");
+        subscriberAdapter.startListening();
+        Log.i(TAG, "subscriptionAdapter: started listening");
+        subscriptionAdapter.startListening();
+    }
 
-        if(listSubscribers.getChildCount()==0)
-            txtDisplay1.setVisibility(View.VISIBLE);
-        else
-            txtDisplay1.setVisibility(View.GONE);
-
-        if(listSubscriptions.getChildCount()==0)
-            txtDisplay2.setVisibility(View.VISIBLE);
-        else
-            txtDisplay2.setVisibility(View.GONE);
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        subscriberAdapter.stopListening();
+        subscriptionAdapter.stopListening();
     }
 }
