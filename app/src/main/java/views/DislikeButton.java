eca9b089eeee82c2b4fa7dc473ea36f5d01ec64e
Mendhie <imendhie@yahoo.com>
2020-02-28 07:43:45 +0100

    Added custom like and dislike button

---
+++ app/src/main/java/views/DislikeButton.java
@@ -0,0 +1,47 @@
+package views;
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+import com.sqube.tipshub.R;
+
+@SuppressLint("AppCompatCustomView")
+public class LikeButton extends ImageView {
+    String TAG = LikeButton.class.getSimpleName();
+
+    public static final int LIKED = 1;
+    public static final int NOT_LIKED = -1;
+
+    private int state;
+    public LikeButton(Context context) {
+        super(context);
+    }
+
+    public LikeButton(Context context, AttributeSet attr){
+        super(context, attr);
+    }
+
+    private void init() {
+        switch (state){
+            case LIKED:
+                setImageResource(R.drawable.ic_thumb_up_color_24dp);
+                break;
+            default:
+                setImageResource(R.drawable.ic_thumb_up_black_24dp);
+                break;
+        }
+    }
+
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+        init();
+    }
+}
