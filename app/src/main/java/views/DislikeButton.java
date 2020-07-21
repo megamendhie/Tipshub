505e148b550997fc4048a080db5bbb9684f47b5c
Mendhie <imendhie@yahoo.com>
2020-07-21 07:50:28 +0100

    Modified like-dislike button functionality

--- app/src/main/java/views/DislikeButton.java
+++ app/src/main/java/views/DislikeButton.java
@@ -9,28 +9,28 @@ import android.widget.ImageView;
 import com.sqube.tipshub.R;
 
 @SuppressLint("AppCompatCustomView")
-public class LikeButton extends ImageView {
-    String TAG = LikeButton.class.getSimpleName();
+public class DislikeButton extends ImageView {
+    String TAG = DislikeButton.class.getSimpleName();
 
-    public static final int LIKED = 1;
-    public static final int NOT_LIKED = -1;
+    public static final int DISLIKED = 1;
+    public static final int NOT_DISLIKED = -1;
 
     private int state;
-    public LikeButton(Context context) {
+    public DislikeButton(Context context) {
         super(context);
     }
 
-    public LikeButton(Context context, AttributeSet attr){
+    public DislikeButton(Context context, AttributeSet attr){
         super(context, attr);
     }
 
     private void init() {
         switch (state){
-            case LIKED:
-                setImageResource(R.drawable.ic_thumb_up_color_24dp);
+            case DISLIKED:
+                setImageResource(R.drawable.ic_thumb_down_color_24dp);
                 break;
             default:
-                setImageResource(R.drawable.ic_thumb_up_black_24dp);
+                setImageResource(R.drawable.ic_thumb_down_black_24dp);
                 break;
         }
     }
